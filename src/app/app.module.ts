94fb0eae99bc6bf395358afb58f0c9bea74b10bf
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-03-31 16:34:43 -0400

    Initial commit & added customer-list & model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // For 2 way binding
 
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule // From above
   ],
   providers: [],
   bootstrap: [AppComponent]
